25460898e15eb71d8657f7b9822b91bfaac2ddac
Daniel_Cordis <cordisdany@gmail.com>
2023-08-01 11:48:00 +0300

    modified

--- TMS.API/TMS.API/Exceptions/EntityNotFoundException.cs
+++ TMS.API/TMS.API/Exceptions/EntityNotFoundException.cs
@@ -1,6 +1,15 @@
-﻿namespace TMS.API.Exceptions
+﻿using System;
+namespace TMS.API.Exceptions
 {
-    public class EntityNotFoundException
-    {
-    }
+        public class EntityNotFoundException : Exception
+        {
+            public EntityNotFoundException() { }
+
+            public EntityNotFoundException(string merrorMessage) : base(merrorMessage) { }
+
+            public EntityNotFoundException(string merrorMessage, Exception innerException) : base(merrorMessage, innerException) { }
+
+            public EntityNotFoundException(long entityId, string entityName) : base(FormattableString.Invariant($"'{entityName}' with id '{entityId}' was not found.")) { }
+        }
+   
 }
